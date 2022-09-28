3a440053e32fb622b066f478c5ee7ebbcf050b9b
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-09-28 16:50:01 +0300

    seems to work

--- Database.cs
+++ Database.cs
@@ -9,7 +9,7 @@ namespace university
 
         public Database(string connectionString)
         {
-            Connection = new NpgsqlConnection(System.Environment.GetEnvironmentVariable("HEROKU_POSTGRESQL_IVORY_URL"));
+            Connection = new NpgsqlConnection(System.Environment.GetEnvironmentVariable("POSTGRE_URL"));
         }
 
         public void Dispose() => Connection.Dispose();
